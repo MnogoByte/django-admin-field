c9ac6e5b524328db91b9a6438f1dbc9991c767f2
meteozond <alex@erix.ru>
2013-04-29 12:57:58 +0400

    + setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+setup(name='Django Admin Field',
+      version='0.0.1',
+      description='Django ModelAdmin field syntax simplifier.',
+      author='Alexander Klimenko',
+      author_email='alex@erix.ru',
+      url='https://github.com/meteozond/django-admin-field',
+      packages=['django_admin_field'], )
