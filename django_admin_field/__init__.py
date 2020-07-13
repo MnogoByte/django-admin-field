3dfd0f3efbd8685891c35b2d3f4b4ab7c5a4504b
Alexander Klimenko <alex@erix.ru>
2020-07-13 18:51:30 +0300

    Added python3 support

--- django_admin_field/__init__.py
+++ django_admin_field/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from functools import reduce
 from django.utils.translation import gettext_lazy as _
 
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 
 setup(name='django-admin-field',
-      version='0.0.1',
+      version='0.0.2',
       description='Django ModelAdmin field syntax simplifier.',
       author='Alexander Klimenko',
       author_email='alex@erix.ru',
